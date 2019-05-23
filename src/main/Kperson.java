03d0d396e56482b6098ba1c88c0b3f629c1c4a6f
muguette <50944437+muguette@users.noreply.github.com>
2019-05-23 11:49:59 +0200

    Update Kperson.java

--- src/main/Kperson.java
+++ src/main/Kperson.java
@@ -14,10 +14,7 @@ public class Kperson  {
 			public void setSalaire(int salaire) {
 				this.salaire = salaire;
 			}
-			@Override
-			public String toString() {
-				return super.toString()+" salaire = "+salaire;
-			}
+			
 			
 	}
 
